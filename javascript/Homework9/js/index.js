09edf74c80d27e2271fdce0fbad072257799a0f3
SabuhiTapdigli <SabuhiTapdigli@gmail.com>
2019-11-18 02:21:14 +0400

    11/18/2019

--- javascript/Homework9/js/index.js
+++ javascript/Homework9/js/index.js
@@ -1,29 +1,9 @@
 const tabsHeader = $('.tabs-header');
 const tabsItems = $ ('.tabs-content-item');
-const tabsItemsContainer = $ ('.tabs-content');
 
-
-tabsHeader.on('click', '.tabs-header-item', function () {
-    tabsItemsContainer.css({
-        height: tabsItemsContainer.height()
-    });
+tabsHeader.on('click', '.tabs-title', function () {
     $('.active').removeClass('active');
     $(this).addClass('active');
     $(tabsItems[$(this).index()]).addClass('active');
 });
 
-
-
-// const tabs = $('.tabs');
-// const tabsItem = $('.tabs-content-item');
-// const tabsContent = $ ('.tabs-content');
-//
-// tabs.on('click', '.tabs-title', function () {
-//     tabsContent.css({
-//         height: tabsContent.height()
-//     });
-//     $('.active').removeClass('active');
-//     $(this).addClass('active');
-//     $(tabsItem[$(this).index()]).addClass('active');
-//
-// });
