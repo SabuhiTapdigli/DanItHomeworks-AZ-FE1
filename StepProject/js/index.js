528e3c7cfec3c49f51390410e6972f80c03095f7
SabuhiTapdigli <SabuhiTapdigli@gmail.com>
2019-11-20 00:52:24 +0400

    11/20/2019

--- StepProject/js/index.js
+++ StepProject/js/index.js
@@ -49,7 +49,24 @@ function loadMoreImages() {
     }
     more.style.display = 'none';
 }
+/*--OPINION--*/
 
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+});
 
 
 
