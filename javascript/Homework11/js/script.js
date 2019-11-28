0d4b8b2562e0620859432b688cc9490ef4f4100d
SabuhiTapdigli <SabuhiTapdigli@gmail.com>
2019-11-29 02:16:56 +0400

    11/29/2019

--- javascript/Homework11/js/script.js
+++ javascript/Homework11/js/script.js
@@ -1,8 +1,54 @@
-let btn = document.getElementsByClassName("btn");
-btn.addEventListener("keypress", myFunction);
-function myFunction(event) {
-    let x = event.which;
-    if (x === 13) {
-       alert ('hello')
-    }
-}
+let btns = document.querySelectorAll('.btn');
+
+window.addEventListener('keydown', event => {
+    switch(event.key) {
+        case 'Enter':
+            btns[0].style.backgroundColor = 'blue';
+            break;
+        case 's':
+            btns[1].style.backgroundColor = 'blue';
+            break;
+        case 'e':
+            btns[2].style.backgroundColor = 'blue';
+            break;
+        case 'o':
+            btns[3].style.backgroundColor = 'blue';
+            break;
+        case 'n':
+            btns[4].style.backgroundColor = 'blue';
+            break;
+        case 'l':
+            btns[5].style.backgroundColor = 'blue';
+            break;
+        case 'z':
+            btns[6].style.backgroundColor = 'blue';
+            break;
+    };
+});
+
+window.addEventListener('keyup', event => {
+    switch (event.key) {
+        case 'Enter':
+            btns[0].style.backgroundColor = '';
+            break;
+        case 's':
+            btns[1].style.backgroundColor = '';
+            break;
+        case 'e':
+            btns[2].style.backgroundColor = '';
+            break;
+        case 'o':
+            btns[3].style.backgroundColor = '';
+            break;
+        case 'n':
+            btns[4].style.backgroundColor = '';
+            break;
+        case 'l':
+            btns[5].style.backgroundColor = '';
+            break;
+        case 'z':
+            btns[6].style.backgroundColor = '';
+            break;
+    };
+});
+
